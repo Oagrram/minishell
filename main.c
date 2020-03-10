6b745a2c6e85fbf35e3c98281b82b9ce8dbb93a9
Othmane Agrram <oagrram@e1r9p4.1337.ma>
2020-03-10 23:35:14 +0100

    add setenv

--- main.c
+++ main.c
@@ -12,6 +12,66 @@
 
 #include "minishell.h"
 
+int		setenv_check(char **line)
+{
+	int i;
+
+	i = -1;
+	if (!ft_isalnum(line[1][0]) && line[1][0] != '_')
+	{
+		ft_putendl("setenv: Variable name must begin with a letter.");
+		return (0);
+	}
+	while (line[1][++i])
+	{
+		if (!ft_isalnum(line[1][i]) && line[1][i] != '_')
+		{
+			ft_putendl("setenv: Variable name must contain alphanumeric characters.");
+			return (0);
+		}
+	}
+	return (1);
+}
+
+void	ft_setenv(t_env *p, char **line)
+{
+	if (!setenv_check(line))
+		return ;
+	while (p->next)
+		p = p->next;
+	if ((p->next = ft_memalloc(sizeof(t_env))) == NULL)
+		exit(1);
+	if ((p->name = ft_strnew(ft_strlen(line[1]))) == NULL)
+		exit(1);
+	p->name = ft_strdup(line[1]);
+	if (line[2])
+	{
+		if ((p->value = ft_strnew(ft_strlen(line[2]))) == NULL)
+			exit(1);
+		p->value = ft_strdup(line[2]);
+	}
+	else
+		p->value = NULL;
+}
+
+int		check_line(char **line, int max, int eror_mes)
+{
+	int i;
+
+	i = -1;
+	while (line[++i])
+		;
+	if (i > max)
+	{
+		if (eror_mes == 1)
+			ft_putendl(": Too many arguments.");
+		else
+			ft_putendl("setenv: Too many arguments.");
+		return (0);
+	}
+	return (1);
+}
+
 int		ft_echo(char **line)
 {
 	int i;
@@ -28,11 +88,14 @@ int		ft_echo(char **line)
 
 int		ft_env(t_env *head)
 {
-	while (head != NULL)
+	while (head->next)
 	{
 		ft_putstr(head->name);
 		ft_putchar('=');
-		ft_putendl(head->value);
+		if (!head->value)
+			ft_putendl("");
+		else
+			ft_putendl(head->value);
 		head = head->next;
 	}
 	return (0);
@@ -42,10 +105,12 @@ int		execut_builtins(char **line, t_env *head)
 {
 	if (!ft_strcmp(line[0], "echo"))
 		ft_echo(line);
-	if (!ft_strcmp(line[0], "cd"))
+	if (!ft_strcmp(line[0], "cd") && check_line(line, 2, 1))
 		ft_cd(head, line);
-	if (!ft_strcmp(line[0], "env"))
+	if (!ft_strcmp(line[0], "env") && check_line(line, 1, 1))
 		ft_env(head);
+	if (!ft_strcmp(line[0], "setenv") && check_line(line, 3, 2))
+		ft_setenv(head, line);
 	return (0);
 }
 
@@ -90,10 +155,10 @@ t_env	*swith_data(char **env, int j)
 		envline = ft_strsplit(env[j], '=');
 		if (envline[1])
 		{
-			if ((p->name = ft_memalloc(ft_strlen(envline[0]))) == NULL)
+			if ((p->name = ft_strnew((ft_strlen(envline[0])))) == NULL)
 				exit(1);
 			p->name = ft_strdup(envline[0]);
-			if ((p->value = ft_memalloc(ft_strlen(envline[1]))) == NULL)
+			if ((p->value = ft_strnew((ft_strlen(envline[1])))) == NULL)
 				exit(1);
 			p->value = ft_strdup(envline[1]);
 			if ((j + 1) != i)
